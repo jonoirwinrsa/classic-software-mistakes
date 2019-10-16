578915e227e10456430f4e5e9bd7ba2f37f5c819
Jonathan Irwin <jono@sov.tech>
2019-10-16 12:43:14 +0200

    feat: mark answered questions by filling in the button
    adds first and last slides
    adds prev button

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -7,18 +7,30 @@ import 'slick-carousel/slick/slick.css'
 export default class Questions extends React.Component {
   state = {
     slideIndex: 0,
-    yes: 0,
-    no: 0
+    answers: []
   }
 
-  answerQuestion (answer) {
+  answerQuestion (answer, index) {
+    const answers = this.state.answers
     if (answer === 'yes') {
-      this.setState({yes: this.state.yes + 1})
-    } else {
-      this.setState({no: this.state.no + 1})
+      answers[index] = 'yes'
+      this.setState({answers})
+    } else if (answer === 'no') {
+      answers[index] = 'no'
+      this.setState({answers})
     }
     this.setState({slideIndex: this.state.slideIndex + 1})
     this.slider.slickGoTo(this.state.slideIndex + 1)
+  }
+
+  reset () {
+    this.setState({slideIndex: 0, answers: []})
+    this.slider.slickGoTo(0)
+  }
+
+  prev () {
+    this.setState({slideIndex: this.state.slideIndex - 1})
+    this.slider.slickGoTo(this.state.slideIndex - 1)
   }
 
   render () {
@@ -29,26 +41,49 @@ export default class Questions extends React.Component {
       arrows: true,
       swipe: false
     }
+    const questions = this.props.pageContext.site.data.questions.questions
+    const {slideIndex, answers} = this.state
 
     return (
       <section id={_.get(this.props, 'section.section_id')} className='block hero-block outer'>
         <div className='inner-small'>
           <div className='block-content'>
             <div className='block-copy'>
-              <div>{this.state.slideIndex + 1}/{this.props.pageContext.site.data.questions.questions.length}</div>
+              <div>{slideIndex}/{questions.length + 1}</div>
               <Slider {...settings} ref={slider => (this.slider = slider)}>
-                {_.map(_.get(this.props, 'pageContext.site.data.questions.questions'), (question, link_idx) => (
-                  <div key={link_idx}>
+                <div>
+                  <h3>There are {questions.length} in this quiz.</h3>
+                  <h4>It should take about 2 minutes to complete and you will be able to see your results right after
+                    completing it.</h4>
+                  <button onClick={() => this.answerQuestion('start', 0)}>Start</button>
+                </div>
+                {questions.map((question, index) => (
+                  <div key={index}>
                     <h3>{_.get(question, 'title')}</h3>
                     <p>{_.get(question, 'description')}</p>
 
                     <div>
-                      <button style={{marginRight: '10px'}} onClick={() => this.answerQuestion('yes')}>Yes</button>
-                      <button onClick={() => this.answerQuestion('no')}>No</button>
+                      <button className={answers[index] === 'yes' && 'filled'}
+                              onClick={() => this.answerQuestion('yes', index)} style={{marginRight: '10px'}}>Yes
+                      </button>
+                      <button className={answers[index] === 'no' && 'filled'}
+                              onClick={() => this.answerQuestion('no', index)}>No
+                      </button>
                     </div>
                   </div>
                 ))}
+                <div>
+                  <h3>Results</h3>
+                  <h3>Yes: {answers.length && answers.filter(answer => answer === 'yes').length}</h3>
+                  <h3>No: {answers.length && answers.filter(answer => answer === 'no').length}</h3>
+                  <button onClick={() => this.reset()}>Reset</button>
+                </div>
               </Slider>
+
+              {slideIndex > 0 &&
+              <div style={{marginTop: '10px'}}>
+                <a onClick={() => this.prev()}>Prev</a>
+              </div>}
             </div>
           </div>
         </div>
